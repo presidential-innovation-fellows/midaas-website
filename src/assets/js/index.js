af0eda46d8cfa8c887b52198fad7e2cf788cba1f
Michael Balint <michael.balint@pif.gov>
2016-02-04 12:54:13 -0500

    Now fetching remote data for chart.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -10,16 +10,22 @@ midaas.indexPage = {
 
   loadChart: function() {
     var labels = [
-      "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "95%",
-      "99%", "100%"
+      "5%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "95%", "99%"
     ];
+    // var data = [[
+    //   9000, 10000, 20000, 30000, 50000, 60000, 70000, 125000, 150000, 175000,
+    //   200000, 300000
+    // ]];
 
-    var data = [[
-      9000, 10000, 20000, 30000, 50000, 60000, 70000, 125000, 150000, 175000,
-      200000, 300000
-    ]];
+    // fetch data remotely...
+    $.getJSON( "https://x0suepap69.execute-api.us-east-1.amazonaws.com/development/income/quantiles", function( data ) {
+      dataArr = [];
+      for (var d in data) {
+        dataArr.push(data[d]);
+      }
+      midaas.chart.createChart(labels, [dataArr]);
+    });
 
-    midaas.chart.createChart(labels, data);
   },
 
   togglePerspective: function(event) {
