7edc7d3d55c0deaeb2b9f504c528b3078d94b3dc
Michael Balint <michael.balint@pif.gov>
2016-02-10 23:59:39 -0500

    Fixed issues with ajax request.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -9,29 +9,29 @@ midaas.indexPage = {
   },
 
   loadChart: function() {
-    var labels = [
-      "5%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "95%", "99%"
-    ];
+    // var labels = [
+    //   "5%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "95%", "99%"
+    // ];
     // var data = [[
     //   9000, 10000, 20000, 30000, 50000, 60000, 70000, 125000, 150000, 175000,
     //   200000, 300000
     // ]];
 
     // fetch data remotely...
-    var dataArr = [],
-        url =  "https://x0suepap69.execute-api.us-east-1.amazonaws.com/development/income/quantiles";
+    var labelArr = [],
+        dataArr = [],
+        url =  "https://brbimhg0w9.execute-api.us-west-2.amazonaws.com/dev/income/quantiles";
 
     $.ajax({
         dataType: "json",
         url: url,
-        data: function(data) {
-          for (var d in data) {
-            dataArr.push(data[d]);
-          }
-        },
         timeout: 10000
-    }).done(function(){
-      midaas.chart.createChart(labels, [dataArr])
+    }).done(function(data){
+      for (var key in data) {
+        labelArr.push(key + "%");
+        dataArr.push(data[key]);
+      }
+      midaas.chart.createChart(labelArr, [dataArr])
     }).fail(function(error){
       midaas.chart.returnError(error)
     });
