7fcbd6f2fec35a34ec944031503f32dae4b0cb7d
Michael Balint <michael.balint@pif.gov>
2016-02-28 11:01:49 -0500

    Formalized grunt compilation process for dependencies and coffeescripts.

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -32,13 +32,32 @@ module.exports = (grunt) ->
     coffee: {
       compile: {
         files: {
-          'dist/assets/js/midaas.js': [
+          'dist/assets/js/midaas-chart.js': [
             'src/assets/coffee/chart.coffee'
             'src/assets/coffee/chart-compare.coffee'
             '!src/assets/coffee/**/_*.*'
             '!src/assets/coffee/**/_*/**'
           ]
         }
+      }
+    }
+
+    concat: {
+      dependencies: {
+        src: [
+          'src/assets/js/vendor/jquery-3.0.0-beta1.min.js'
+          'src/assets/js/vendor/d3-3.5.14.min.js'
+          'src/assets/js/vendor/c3-0.4.10.min.js'
+          'src/assets/js/vendor/observe.js'
+        ]
+        dest: 'dist/assets/js/midaas-dependencies.js'
+      }
+      midaas: {
+        src: [
+          'dist/assets/js/midaas-dependencies.js'
+          'dist/assets/js/midaas-chart.js'
+        ]
+        dest: 'dist/assets/js/midaas.js'
       }
     }
 
@@ -69,7 +88,8 @@ module.exports = (grunt) ->
           '**/*'
           '!**/*.scss'
           '!**/*.jade'
-          '!**/coffee/**'
+          '!**/assets/coffee/**'
+          '!**/assets/js/**'
           '!**/_*.*'
           '!**/_*/**'
         ]
@@ -95,9 +115,13 @@ module.exports = (grunt) ->
         files: ['src/**/*.jade']
         tasks: ['jade']
       }
+      dependencies: {
+        files: ['src/assets/js/vendor/**/*.js']
+        tasks: ['concat:dependencies', 'concat:midaas']
+      }
       coffee: {
         files: ['src/assets/coffee/**/*.coffee']
-        tasks: ['coffee']
+        tasks: ['coffee', 'concat:midaas']
       }
       sass: {
         files: ['src/**/*.scss']
@@ -136,15 +160,17 @@ module.exports = (grunt) ->
   # LIBRARIES
   #
 
+  grunt.loadNpmTasks('grunt-aws')
   grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-coffee')
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect')
   grunt.loadNpmTasks('grunt-contrib-copy')
   grunt.loadNpmTasks('grunt-contrib-jade')
-  grunt.loadNpmTasks('grunt-contrib-coffee')
-  grunt.loadNpmTasks('grunt-sass')
-  grunt.loadNpmTasks('grunt-newer')
-  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch')
-  grunt.loadNpmTasks('grunt-aws')
+  grunt.loadNpmTasks('grunt-newer')
+  grunt.loadNpmTasks('grunt-sass')
 
   #
   # TASKS
@@ -153,7 +179,9 @@ module.exports = (grunt) ->
   grunt.registerTask('default', [
     'clean'
     'jade'
+    'concat:dependencies'
     'coffee'
+    'concat:midaas'
     'sass'
     'copy'
     'connect:server'
@@ -163,7 +191,9 @@ module.exports = (grunt) ->
   grunt.registerTask('deploy', [
     'clean'
     'jade'
+    'concat:dependencies'
     'coffee'
+    'concat:midaas'
     'sass'
     'copy'
     's3'
---
+++ dist/assets/js/midaas-chart.js
@@ -0,0 +1,194 @@
+(function() {
+  var Chart;
+
+  Chart = (function() {
+    function Chart() {}
+
+    Chart.prototype.showLoading = function() {
+      return $(this.chartId + " #loading-icon").fadeIn("fast");
+    };
+
+    Chart.prototype.hideLoading = function() {
+      return $(this.chartId + " #loading-icon").fadeOut("fast");
+    };
+
+    Chart.prototype.apiUrlBase = "https://brbimhg0w9.execute-api.us-west-2.amazonaws.com/dev/";
+
+    Chart.prototype.fetchData = function(callback) {
+      var data, err;
+      err = null;
+      data = null;
+      return callback(err, data);
+    };
+
+    Chart.prototype.init = function() {
+      var bindElement;
+      this.showLoading();
+      bindElement = this.chartId + " .chart";
+      return this.fetchData((function(_this) {
+        return function(err, data) {
+          _this._chart = c3.generate({
+            bindto: bindElement,
+            data: {
+              x: "x",
+              columns: data,
+              type: "bar"
+            },
+            bar: {
+              width: {
+                ratio: 0.7
+              }
+            },
+            axis: {
+              x: {
+                type: "category"
+              }
+            }
+          });
+          return _this.hideLoading();
+        };
+      })(this));
+    };
+
+    Chart.prototype.update = function() {
+      this.showLoading();
+      return this.fetchData((function(_this) {
+        return function(err, data) {
+          _this._chart.load({
+            columns: data,
+            unload: _this._chart.columns
+          });
+          return _this.hideLoading();
+        };
+      })(this));
+    };
+
+    return Chart;
+
+  })();
+
+  window.Chart = Chart;
+
+}).call(this);
+
+(function() {
+  var ChartCompare,
+    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
+  ChartCompare = (function(superClass) {
+    extend(ChartCompare, superClass);
+
+    function ChartCompare(chartId1) {
+      var chartEl, chartTemplate, ref, ref1;
+      this.chartId = chartId1;
+      this.fetchData = bind(this.fetchData, this);
+      this.getApiUrl = bind(this.getApiUrl, this);
+      chartEl = $(this.chartId);
+      this.query = {
+        compare: (ref = chartEl.attr('compare')) != null ? ref : "Overall",
+        compareRegion: (ref1 = chartEl.attr('compare-region')) != null ? ref1 : "US"
+      };
+      chartTemplate = "/assets/templates/chart-compare.html";
+      $("" + this.chartId).load(chartTemplate, null, (function(_this) {
+        return function() {
+          _this.setTitle(_this.chartId);
+          _this._bindToggles();
+          return _this.init();
+        };
+      })(this));
+    }
+
+    ChartCompare.prototype.getApiUrl = function() {
+      return this.apiUrlBase + "income/quantiles";
+    };
+
+    ChartCompare.prototype.fetchData = function(callback) {
+      var params, url;
+      params = [];
+      url = this.getApiUrl();
+      switch (this.query.compare.toLowerCase()) {
+        case "race":
+          params.push("compare=race");
+          break;
+        case "gender":
+        case "sex":
+          params.push("compare=sex");
+          break;
+        case "age":
+          params.push("compare=agegroup");
+      }
+      if ((this.query.compareRegion != null) && this.query.compareRegion !== "US") {
+        params.push("state=" + this.query.compareRegion);
+      }
+      if (params.length) {
+        url += "?" + params.join('&');
+      }
+      return $.ajax({
+        dataType: "json",
+        url: url,
+        timeout: 10000
+      }).done((function(_this) {
+        return function(data) {
+          var dataArr, group, labelArr, percentile, seriesArr;
+          seriesArr = [];
+          for (group in data) {
+            labelArr = ["x"];
+            dataArr = [group];
+            for (percentile in data[group]) {
+              labelArr.push(percentile);
+              dataArr.push(data[group][percentile]);
+            }
+            seriesArr.push(dataArr);
+          }
+          return callback(null, [labelArr].concat(seriesArr));
+        };
+      })(this)).fail((function(_this) {
+        return function(err) {
+          return callback(err);
+        };
+      })(this));
+    };
+
+    ChartCompare.prototype.setTitle = function(chartId) {
+      var chartEl, title;
+      chartEl = $(this.chartId);
+      title = chartEl.attr("title");
+      return chartEl.find(".chart-title").text(title);
+    };
+
+    ChartCompare.prototype._bindToggles = function() {
+      $(this.chartId + " .toggle").on("click", (function(_this) {
+        return function(event) {
+          _this.query.compare = event.currentTarget.innerText;
+          _this.update();
+          $(_this.chartId + " .toggles li").removeClass("active");
+          return $(event.target).addClass("active");
+        };
+      })(this));
+      return $(this.chartId + " #region-selector").change((function(_this) {
+        return function(event) {
+          _this.query.compareRegion = event.target.value;
+          return _this.update();
+        };
+      })(this));
+    };
+
+    return ChartCompare;
+
+  })(Chart);
+
+  window.ChartCompare = ChartCompare;
+
+  $(document).ready((function(_this) {
+    return function() {
+      return $('.chart-compare').each(function(i) {
+        var chartCompare, id;
+        id = '#' + $(this).attr('id');
+        return chartCompare = new ChartCompare(id);
+      });
+    };
+  })(this));
+
+}).call(this);
